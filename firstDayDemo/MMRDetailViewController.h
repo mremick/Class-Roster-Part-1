736022ba560115cb8e0886d5e41b3be5bd6732e7
Matt Remick <mremick@gmail.com>
2014-01-13 13:37:54 -0800

    Finished the project

---
+++ firstDayDemo/MMRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMRDetailViewController.h
+//  firstDayDemo
+//
+//  Created by Matt Remick on 1/13/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMRDetailViewController : UIViewController
+
+@end
---
+++ firstDayDemo/MMRDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  MMRDetailViewController.m
+//  firstDayDemo
+//
+//  Created by Matt Remick on 1/13/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import "MMRDetailViewController.h"
+
+@interface MMRDetailViewController ()
+
+@end
+
+@implementation MMRDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
